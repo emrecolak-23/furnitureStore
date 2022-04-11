956013c6cf7721c46db0185732dcf1af81d1d5c2
Emre Çolak <colakkemre@gmail.com>
2022-04-11 15:33:06 +0300

    multer installed and category image upload middlewares added into categoryroutes

--- routes/CategoryRoutes.js
+++ routes/CategoryRoutes.js
@@ -1,12 +1,25 @@
 // Import Packages
 const express = require('express');
-
+const multer = require('multer');
 // Import Controllers
 const CategoryController = require('../controllers/CategoryController');
 
 // Create express router
 const router = express.Router();
 
-router.route('/').post(CategoryController.createCategory);
+// Multer Middlewares
+let storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, __dirname + '/../uploads');
+  },
+  filename: function (req, file, callback) {
+    callback(null, file.fieldname + '_' + Date.now() + '_' + file.originalname);
+  },
+});
+let upload = multer({
+  storage: storage,
+}).single('image');
+
+router.route('/').post(upload, CategoryController.createCategory);
 
 module.exports = router
