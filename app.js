6d3f60791dc07f4276ff4024a61be131cea53197
Emre Çolak <colakkemre@gmail.com>
2022-04-11 14:10:36 +0300

    db connected completed

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 // Import Packages
 const express = require('express');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
 
 // Import Routes
 const PageRouter = require('./routes/PageRoutes');
 
 // Create express app
 const app = express();
+
+// DB Connect
+const dbURI =
+  'mongodb+srv://emco:emco3232@nodetuts.iuulr.mongodb.net/furniture-store?retryWrites=true&w=majority';
+mongoose
+  .connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((result) => {
+    console.log('Db connected');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 
 // Template Engine
 app.set('view engine', 'ejs');
