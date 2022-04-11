0c6f497af481d64402cf7b60f5f8f38ad095ac16
Emre Çolak <colakkemre@gmail.com>
2022-04-11 13:53:27 +0300

    controller and routes created, also parse pages into partials

--- controllers/PageController.js
+++ controllers/PageController.js
@@ -1,3 +1,29 @@
 exports.getHomePage = (req, res) => {
-  res.status(200).render('index');
+  res.status(200).render('index', {
+    page_name: 'index'
+  });
+}
+
+exports.getFurniturePage = (req, res) => {
+  res.status(200).render('furnitures', {
+    page_name: 'furnitures'
+  });
+}
+
+exports.getContactPage = (req, res) => {
+  res.status(200).render('contact', {
+    page_name: 'contact'
+  });
+}
+
+exports.getBlogPage = (req, res) => {
+  res.status(200).render('blog', {
+    page_name: 'blog'
+  });
+}
+
+exports.getAboutPage = (req, res) => {
+  res.status(200).render('about', {
+    page_name: 'about'
+  });
 }
--- routes/PageRoutes.js
+++ routes/PageRoutes.js
@@ -7,6 +7,19 @@ const router = express.Router();
 // Import Controllers
 const PageController = require('../controllers/PageController');
 
+// Home Page
 router.route('/').get(PageController.getHomePage);
+
+// Furniture Page
+router.route('/furnitures').get(PageController.getFurniturePage);
+
+// Contact page
+router.route('/contact').get(PageController.getContactPage);
+
+// Blog Page
+router.route('/blog').get(PageController.getBlogPage);
+
+// About Page
+router.route('/about').get(PageController.getAboutPage);
 
 module.exports = router;
